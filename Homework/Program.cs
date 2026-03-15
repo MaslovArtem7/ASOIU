454a1812771ee33e66ce49853bbfe18a420096d8
Artyom <Artyom@LAPTOP-5QP29IVR>
2026-03-15 13:57:11 +0300

    homework

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,70 +1,65 @@
-﻿using System;
-//Маслов Артем ИУ5-22Б
-class DamerauLevenstein
+﻿namespace ConsoleApp1
 {
-    public static int Distance(string str1Param, string str2Param)
+    internal class Program
     {
-        if ((str1Param == null) || (str2Param == null)) return -1;
-        int str1Len = str1Param.Length;
-        int str2Len = str2Param.Length;
-        //Если хотя бы одна строка пустая,
-        //возвращается длина другой строки
-        if ((str1Len == 0) && (str2Len == 0)) return 0;
-        if (str1Len == 0) return str2Len;
-        if (str2Len == 0) return str1Len;
-        //Приведение строк к верхнему регистру
-        string str1 = str1Param.ToUpper();
-        string str2 = str2Param.ToUpper();
-        //Объявление матрицы
-        int[,] matrix = new int[str1Len + 1, str2Len + 1];
-        //Инициализация нулевой строки и нулевого столбца матрицы
-        for (int i = 0; i <= str1Len; i++) matrix[i, 0] = i;
-        for (int j = 0; j <= str2Len; j++) matrix[0, j] = j;
-        //Вычисление расстояния Дамерау-Левенштейна
-        for (int i = 1; i <= str1Len; i++)
+        public static int Distance(string str1Param, string str2Param)
         {
-            for (int j = 1; j <= str2Len; j++)
+            if ((str1Param == null) || (str2Param == null)) return -1;
+            int str1Len = str1Param.Length;
+            int str2Len = str2Param.Length;
+
+            if ((str1Len == 0) & (str2Len == 0)) return 0;
+            if (str1Len == 0) return str2Len;
+            if (str2Len == 0) return str1Len;
+
+            string str1 = str1Param.ToUpper();
+            string str2 = str2Param.ToUpper();
+
+            int[,] matrix = new int[str1Len + 1, str2Len + 1];
+            for (int i = 0; i <= str1Len; i++) matrix[i, 0] = i;
+            for (int j = 0; j <= str2Len; j++) matrix[0, j] = j;
+
+            for (int i = 1; i <= str1Len; i++)
             {
-                //Эквивалентность символов, переменная symbEqual
-                //соответствует m(s1[i],s2[j])
-                int symbEqual = (
-                (str1.Substring(i - 1, 1) ==
-                str2.Substring(j - 1, 1)) ? 0 : 1);
-                int ins = matrix[i, j - 1] + 1; //Добавление
-                int del = matrix[i - 1, j] + 1; //Удаление
-                int subst = matrix[i - 1, j - 1] + symbEqual; //Замена
-                                                              //Элемент матрицы вычисляется
-                                                              //как минимальный из трех случаев
-                matrix[i, j] = Math.Min(Math.Min(ins, del), subst);
-                //Дополнение Дамерау по перестановке соседних символов
-                if ((i > 1) && (j > 1) &&
-
-
-                (str1.Substring(i - 1, 1) == str2.Substring(j - 2, 1)) &&
-                (str1.Substring(i - 2, 1) == str2.Substring(j - 1, 1)))
+                for (int j = 1; j <= str2Len; j++)
                 {
-                    matrix[i, j] = Math.Min(matrix[i, j],
-                    matrix[i - 2, j - 2] + symbEqual);
+                    int SymbEqual = (
+                        (str1.Substring(i - 1, 1) == str2.Substring(j - 1, 1)) ? 0 : 1);
+                    int ins = matrix[i, j - 1] + 1;
+                    int del = matrix[i - 1, j] + 1;
+                    int rep = matrix[i - 1, j - 1] + SymbEqual;
+
+                    matrix[i, j] = Math.Min(del, Math.Min(ins, rep));
+
+                    if ((i > 1) && (j > 1) &&
+                        (str1.Substring((i - 1), 1) == str2.Substring((j - 2), 1)) &&
+                        (str1.Substring((i - 2), 1) == str2.Substring((j - 1), 1)))
+                    {
+                        matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + SymbEqual);
+                    }
                 }
             }
-        }
-        //Возвращается нижний правый элемент матрицы
-        return matrix[str1Len, str2Len];
-    }
-    /// Вывод расстояния Дамерау-Левенштейна в консоль
-    public static void WriteDistance(string str1Param, string str2Param)
-    {
-        int d = Distance(str1Param, str2Param);
-        Console.WriteLine("'" + str1Param + "','" +
-        str2Param + "' -> " + d.ToString());
-    }
-    static void Main()
-    {
-        //Сделал пользовательский ввод
-        Console.WriteLine("Введите два слова для проверки");
-        string str1 = Console.ReadLine();
-        string str2 = Console.ReadLine();
 
-        DamerauLevenstein.WriteDistance(str1, str2);
+            return matrix[str1Len, str2Len];
+        }
+        public static void WriteDistance(string str1Param, string str2Param)
+        {
+            int a = Distance(str1Param, str2Param);
+            Console.WriteLine($"{str1Param}, {str2Param} -> {a}");
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1 строку (для выхода напишите exit): ");
+            string? s1 = Console.ReadLine();
+            while ((s1 != null) && (s1 != "exit"))
+            {
+                Console.WriteLine("Введите 2 строку: ");
+                string? s2 = Console.ReadLine();
+                if (s2 == null) break;
+                WriteDistance(s1, s2);
+                Console.WriteLine("Введите 1 строку (для выхода напишите exit): ");
+                s1 = Console.ReadLine();
+            }
+        }
     }
 }
